6920a659cee72686b60358281f71f6a677eb5e13
Andrzej Ressel <jereksel@gmail.com>
2018-01-30 22:22:45 +0100

    Working on faster builds

--- detekt.gradle.kts
+++ detekt.gradle.kts
@@ -6,21 +6,18 @@ repositories {
 val detektConf by configurations.creating
 
 dependencies {
-    detektConf("io.gitlab.arturbosch.detekt:detekt-cli:1.0.0.M13")
-    detektConf("io.gitlab.arturbosch.detekt:detekt-formatting:1.0.0.M13")
+    detektConf("io.gitlab.arturbosch.detekt:detekt-cli:1.0.0.RC6-2")
 }
-
 val detekt by tasks.creating(JavaExec::class) {
     group = "verification"
     main = "io.gitlab.arturbosch.detekt.cli.Main"
     classpath = detektConf
     val input = "${project.projectDir.absolutePath}"
     val config = "${project.projectDir}/detekt.yml"
-    val reports = "${project.projectDir.absolutePath}/reports/report.detekt"
+    val reports = "${project.projectDir.absolutePath}/reports/"
     val baseline = "${project.projectDir.absolutePath}/reports/baseline.xml"
-    val filters = ".*test.*"
-    val rulesets = ""
-    val params = listOf("-p", input, "-c", config, "-f", filters, "-r", rulesets, "-o", reports, "-b", baseline)
+    val filters = ".*test.*, .*/build/.*"
+    val params = listOf("-i", input, "-c", config, "-f", filters, "-o", reports, "-b", baseline, "--parallel")
     args(params)
 }
 
@@ -30,10 +27,9 @@ val detektEstablishAcceptedErrors by tasks.creating(JavaExec::class) {
     classpath = detektConf
     val input = "${project.projectDir.absolutePath}"
     val config = "${project.projectDir}/detekt.yml"
-    val reports = "${project.projectDir.absolutePath}/reports/report.detekt"
+    val reports = "${project.projectDir.absolutePath}/reports/"
     val baseline = "${project.projectDir.absolutePath}/reports/baseline.xml"
-    val filters = ".*test.*"
-    val rulesets = ""
-    val params = listOf("-p", input, "-c", config, "-f", filters, "-r", rulesets, "-o", reports, "-b", baseline, "-cb")
+    val filters = ".*test.*, .*/build/.*"
+    val params = listOf("-i", input, "-c", config, "-f", filters, "-o", reports, "-b", baseline, "-cb", "--parallel")
     args(params)
 }
