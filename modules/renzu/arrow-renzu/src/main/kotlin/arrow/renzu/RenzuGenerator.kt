cb4d81153624c1ca194941fca038ba9de426b128
Andrzej Ressel <jereksel@gmail.com>
2018-07-10 20:13:57 +0200

    Update

--- modules/renzu/arrow-renzu/src/main/kotlin/arrow/renzu/RenzuGenerator.kt
+++ modules/renzu/arrow-renzu/src/main/kotlin/arrow/renzu/RenzuGenerator.kt
@@ -11,11 +11,16 @@ fun main(args: Array<String>) {
     val classes = ClassPath.from(ListK::class.java.classLoader).allClasses
             .asSequence()
             .filter { it.packageName.startsWith("arrow") }
-            .map { it.load() }
-            .map { it to it.getAnnotationsByType(higherkind::class.java).toList() }
-            .filter { it.second.isNotEmpty() }
-            .map { it.first }
+//            .map { it.load() }
+//            .map { it to it.getAnnotationsByType(higherkind::class.java).toList() }
+//            .filter { it.second.isNotEmpty() }
+//            .map { it.first }
+            .filter { it.packageName.contains("typeclass") }
+            .filterNot { it.toString().contains("$") }
+            .filterNot { it.toString().contains("_") }
+            .filter { Try {it.load().isInterface}.getOrDefault { false } }
             .toList()
+            .sortedBy { it.toString() }
 //            .map { Try { it.kotlin.annotations.any { it.javaClass == higherkind::class.java } } }
 //            .mapNotNull { it.getOrDefault { null } }
 //            .filter { it }
